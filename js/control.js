46092238d2bd14a3aefbef0746da355107debbec
Hancel <imlinhanchao@gmail.com>
2015-06-06 16:17:27 +0800

    V0.2

--- js/control.js
+++ js/control.js
@@ -1,3 +1,15 @@
+
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 console.log("Begin...");
 var curPage = location.pathname;
 switch(curPage)
@@ -6,39 +18,99 @@ switch(curPage)
 		getProblemset(2);
 		break;
 	case "/problemset.page":
-		getProblemset(3);
+		getProblem(3);
 		break;
 	case "/problem.page":
-		getProblem();
+		getInput();
 		break;
 	default:
-		if(curPage.length > 2) window.close();
+		//if(curPage.length > 2) window.close();
 		break;
 }
 
-function getProblemset(step)
+function mathLink(link, reg)
 {
+	return link.search(reg) >= 0;
+}
+
+// 获取页面列表的所有符合规则的链接
+function getLinks(reg)
+{
+	var lstLink = [];
+	var count = 0;
 	var tb = document.getElementsByClassName("table")[0];
-	var lstProblemset = tb.getElementsByTagName("a");
-	for(var i = 0; i < lstProblemset.length; i += step)
+	var lstA = tb.getElementsByTagName("a");
+	for(var i = 0; i < lstA.length; i++)
 	{
-		window.open(lstProblemset[i].href, "_blank");
-		console.log("Open " + lstProblemset[i].href);
+		if(!lstLink.contains(lstA[i].href) && mathLink(lstA[i].href, reg))
+			lstLink[count++] = lstA[i].href;
 	}
-	window.close();
+	return lstLink;
+}
+
+// 创建一个隐藏的iframe
+function createIframe(name)
+{
+	var ifr = document.createElement("iframe");
+	ifr.style.display = "none";
+	ifr.name = name;
+	ifr.id = name;
+	ifr.src = "";
+	document.body.appendChild(ifr);
+}
+
+// 在隐藏iframe中打开链接
+function openByFrame(link)
+{
+	var ifrName = "lanqiaoEx_iframe_open_window";
+	var ifr = document.getElementById(ifrName);
+	if(null == ifr) createIframe(ifrName);
+	return window.open(link, "_blank");
+}
+
+// 打开下一个页面
+function openNext()
+{
+	if(_curLink == _lstLink.length && opener)
+	{
+		opener.openNext();
+		window.close();
+	}
+	else if(_curLink == _lstLink.length)
+	{
+		alert("下载完成！立即查看>>>");
+		chrome.downloads.showDefaultFolder();
+	}
+	openByFrame(_lstLink[_curLink++]);
+}
+
+// 试题集页面执行的链接
+function getProblemset()
+{
+	_lstLink = getLinks(/problemset\.page/);
+	_curLink = 0;
+	openNext();
 }
 
 function getProblem()
 {
+	console.log(location.href);
+	_lstLink = getLinks(/problem\.page/);
+	_curLink = 0;
+	openNext();
+}
+
+function getInput()
+{
+	console.log(location.href);
 	var content = document.getElementsByClassName("des")[0].innerHTML;
 	var title = document.getElementsByClassName("tit")[0].innerHTML;
 	var search = getArgs();
 	content = "<meta charset='utf-8'/>" + content;
 	content = "<title>" + title + "</title>" + content;
 	downloadFile("Problem/" + search.gpid + ".html", content);
-	var filename = "input-" + gpid + "-" + did + ".txt";
  	for(var i = 1; i <= 10; i++)
-		downloadInput(gpid, i);
+		downloadInput(search.gpid, i);
 }
 
 function downloadFile(fileName, content){
@@ -72,9 +144,16 @@ function downloadInput(pid, did)
 {
 	var filename = "input-" + pid + "-" + did + ".txt";
  	$.post("http://lx.lanqiao.org/lanqiao.DownloadData.dt", {type:"inp",gpid:pid,idx:did}, function(obj) {
+		if(undefined == obj["handle"])
+		{
+			opener.openNext();
+		}
 		$.get("http://lx.lanqiao.org/web.RequireTempFile.do?handle=" + obj["handle"], function(content){
 			downloadFile(filename, content);
-			if(did == 10) window.close();
+			if(did == 10)
+			{
+				opener.openNext();
+			}
 		});
 	}, "JSON");
 }
